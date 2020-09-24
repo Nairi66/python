2fa30fe7577e61a23a2e5bfe6a392fb064fca9a6
Nairi66 <69626762+Nairi66@users.noreply.github.com>
2020-09-24 08:50:10 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-import telebot, requests                                                                                                                                           
+import telebot, requests 
+from telebot import apihelper
+import os
+import time
+import logging                                                                                                                                          
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.DEBUG)
+
+TG_PROXY = 'https://103.241.156.250:8080'
+apihelper.proxy = {'http': TG_PROXY}
+bot = telebot.TeleBot("1292521216:AAFG73G8bmWVEsSVM8JRqEyGNuZsfz23Cig")
 
 bot = telebot.TeleBot("1292521216:AAFG73G8bmWVEsSVM8JRqEyGNuZsfz23Cig")
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.send_message(message.chat.id, "👋")
+    bot.send_message(message.chat.id, "Ողջոյն, բարի գալուս յղումների կրճատման համակարգ։ ❤️❤️")
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    # bot.send_message(message.chat.id, "👋")
+    bot.send_message(message.chat.id, "Յղում կրճատելու համար անհրաժեշ ՝\n1․ գրել @short եւ անհրաժեշտ յղումը\n2. պատճենէլ ստացուած կրճատ յղումը եւ աւգտագործել։")
 
 @bot.message_handler(content_types=['text'])
 def url_shortener(message):
@@ -15,7 +32,7 @@ def url_shortener(message):
         x = requests.post(post_url, data = data)
         short_key = x.text[:7]
         short_url = "նա.հայ/?id=" + short_key
-        text = "Your short url is there. Thank you!!! ❤️❤️\n"
+        text = "Ձեր կրճատած յղումը պատրաստ է։ Շնորհակալութիւն։ ❤️❤️\n"
         bot.send_message(message.chat.id, text+short_url)
 
 bot.polling()
